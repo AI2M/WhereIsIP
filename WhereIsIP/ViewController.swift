22c9b9a5c56a630de24a22cc28c1de6cc25c5b5e
Tongchai Tonsau <tongchaitonsau@Tongchais-MacBook-Pro.local>
2017-04-27 15:29:42 +0700

    Initial Commit

---
+++ WhereIsIP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereIsIP
+//
+//  Created by Tongchai Tonsau on 4/27/2560 BE.
+//  Copyright © 2560 Tongchai Tonsau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
